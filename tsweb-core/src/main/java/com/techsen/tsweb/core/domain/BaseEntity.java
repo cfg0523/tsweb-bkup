f59ad0b61ea4bd6b1de95764f87d7c06e29f76ed
cfg0523 <cfg0523@sina.com>
2015-05-13 14:59:21 +0800

    1. tsweb-core: add class BaseObject 2. tsweb-core: add class BaseEntity(extends BaseObject)

---
+++ tsweb-core/src/main/java/com/techsen/tsweb/core/domain/BaseEntity.java
@@ -0,0 +1,117 @@
+package com.techsen.tsweb.core.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 所有业务实体对象的基类
+ */
+@SuppressWarnings("serial")
+public abstract class BaseEntity<T> extends BaseObject<T> {
+
+    /**
+     * ID采用Serializable类型，是为了能与其他非String类型的设计兼容，
+     * 推荐采用32位字符串类型的UUID值
+     */
+    protected Serializable id;
+    protected String createBy;
+    protected Date createDate;
+    protected String updateBy;
+    protected Date updateDate;
+    protected String remark;
+
+    /**
+     * 公共的hashCode方法
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    /**
+     * 公共的equals方法
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        BaseEntity<T> other = (BaseEntity<T>) obj;
+        if (this.id == null) {
+            return other.id == null;
+        } else {
+            return this.id.equals(other.id);
+        }
+    }
+
+    public Serializable getId() {
+        return id;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T setId(Serializable id) {
+        this.id = id;
+        return (T) this;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T setCreateBy(String createBy) {
+        this.createBy = createBy;
+        return (T) this;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T setCreateDate(Date createDate) {
+        this.createDate = createDate;
+        return (T) this;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+        return (T) this;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+        return (T) this;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    @SuppressWarnings("unchecked")
+    public T setRemark(String remark) {
+        this.remark = remark;
+        return (T) this;
+    }
+
+}
---
+++ tsweb-core/src/main/java/com/techsen/tsweb/core/domain/BaseObject.java
@@ -0,0 +1,29 @@
+package com.techsen.tsweb.core.domain;
+
+import java.io.Serializable;
+
+import com.techsen.tsweb.core.util.ObjectUtil;
+
+/**
+ * 一个对象基类，提供公共的
+ */
+@SuppressWarnings("serial")
+public abstract class BaseObject<T> implements Serializable {
+
+    /**
+     * 公共的toString()方法，供子类继承
+     */
+    @Override
+    public String toString() {
+        return ObjectUtil.toString(this);
+    }
+
+    /**
+     * 公共的clone方法，供子类继承
+     */
+    @Override
+    protected T clone() {
+        return ObjectUtil.clone(this);
+    };
+
+}

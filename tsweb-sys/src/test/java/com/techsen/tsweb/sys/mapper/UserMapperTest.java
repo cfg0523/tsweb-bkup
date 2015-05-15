2d9174dc71566827b320bddaeeba0412a789152d
cfg0523 <cfg0523@sina.com>
2015-05-15 16:52:31 +0800

    test UserDao function

--- tsweb-sys/src/test/java/com/techsen/tsweb/sys/mapper/UserMapperTest.java
+++ tsweb-sys/src/test/java/com/techsen/tsweb/sys/mapper/UserMapperTest.java
@@ -7,7 +7,9 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.techsen.tsweb.core.util.UUIDUtil;
 import com.techsen.tsweb.sys.dao.UserDao;
+import com.techsen.tsweb.sys.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:META-INF/**/spring-beans.xml")
@@ -17,8 +19,39 @@ public class UserMapperTest {
 
     @Test
     public void testGetEntity() {
-        System.out.println("--------------------");
         System.out.println(userDao.getEntity("1"));
-        System.out.println("--------------------");
+    }
+    
+    @Test
+    public void testAddEntity() {
+        User user = new User();
+        
+        user.setId(UUIDUtil.uuid());
+        user.setUsername("cfg0523");
+        user.setPassword("qiannianhu");
+        user.setRemark("test");
+        
+        userDao.addEntity(user);
+    }
+    
+    @Test
+    public void testUpdateEntity() {
+        User user = new User();
+        
+        user.setId("f0e731ed841c4e77866a9e31580eef57");
+        user.setUsername("cfg0523");
+        user.setPassword("qiannianhu");
+        user.setRemark("test by hayden");
+        
+        userDao.updateEntity(user);
+    }
+    
+    @Test
+    public void testDeleteEntity() {
+        User user = new User();
+        
+        user.setId("f0e731ed841c4e77866a9e31580eef57");
+        
+        userDao.deleteEntity(user);
     }
 }
